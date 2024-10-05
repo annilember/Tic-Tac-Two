7f378685f5fc48aa33a40e76074b06235a27f00b
Anni Lember <annlem@taltech.ee>
2024-10-05 14:08:01 +0300

    after many attempts, created a new working Solution for Tic-Tac-Two, hopefully

---
+++ Tic-Tac-Two/ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿// An example of bad design, fine for UI prototyping.
+using MenuSystem;
+
+var deepMenu = new Menu(
+    EMenuLevel.Deep,
+    "TIC-TAC-TOE Deep stuff...", [
+        new MenuItem()
+        {
+            Shortcut = "Y",
+            Title = "Why is the question.",
+            MenuItemAction = DummyMethod
+        }
+    ]);
+var optionsMenu = new Menu(
+    EMenuLevel.Secondary,
+    "TIC-TAC-TOE Options", [
+        new MenuItem()
+        {
+            Shortcut = "X",
+            Title = "X Starts",
+            MenuItemAction = deepMenu.Run
+        },
+
+        new MenuItem()
+        {
+            Shortcut = "O",
+            Title = "O Starts",
+            MenuItemAction = DummyMethod
+        }
+    ]);
+
+var mainMenu = new Menu(
+    EMenuLevel.Main,
+    "TIC-TAC-TOE", [
+        new MenuItem()
+        {
+            Shortcut = "O",
+            Title = "Options",
+            MenuItemAction = optionsMenu.Run
+        },
+
+        new MenuItem()
+        {
+            Shortcut = "N",
+            Title = "New Game",
+            MenuItemAction = DummyMethod
+        }
+    ]);
+
+mainMenu.Run();
+
+return;
+// ============================================
+
+string DummyMethod()
+{
+    Console.Write("Press any key to exit...");
+    Console.ReadKey();
+    return "Dummy";
+}
---
+++ Tic-Tac-Two/MenuSystem/Menu.cs
@@ -0,0 +1,139 @@
+namespace MenuSystem;
+
+public class Menu
+{
+    private string MenuHeader { get; set; }
+    private static string _menuDivider = "======================";
+    private List<MenuItem> MenuItems { get; set; }
+    private MenuItem _menuItemExit = new MenuItem()
+    {
+        Shortcut = "E",
+        Title = "Exit",
+        MenuItemAction = null
+    };
+    private MenuItem _menuItemReturn = new MenuItem()
+    {
+        Shortcut = "R",
+        Title = "Return",
+        MenuItemAction = null
+    };
+    private MenuItem _menuItemReturnMain = new MenuItem()
+    {
+        Shortcut = "M",
+        Title = "Return to Main Menu",
+        MenuItemAction = null
+    };
+    private EMenuLevel _menuLevel { get; set; }
+    
+    public Menu(EMenuLevel menuLevel, string menuHeader, List<MenuItem> menuItems)
+    {
+        if (string.IsNullOrWhiteSpace(menuHeader))
+        {
+            throw new ApplicationException("Menu header cannot be empty.");
+        }
+        
+        MenuHeader = menuHeader;
+
+        if (menuItems == null || menuItems.Count == 0)
+        {
+            throw new ApplicationException("Menu items cannot be empty.");
+        }
+        
+        MenuItems = menuItems;
+        
+        _menuLevel = menuLevel;
+        
+        if (_menuLevel != EMenuLevel.Main)
+        {
+            MenuItems.Add(_menuItemReturn);
+        }
+        if (_menuLevel == EMenuLevel.Deep)
+        {
+            MenuItems.Add(_menuItemReturnMain);
+        }
+        MenuItems.Add(_menuItemExit);
+        
+        //TODO! Validate menuItems for shortcut conflict!
+    }
+
+    public string Run()
+    {
+        Console.Clear();
+        //TODO! Implement main menu loop!
+
+        do
+        {
+            var menuItem = DisplayMenuGetUserChoice();
+            var menuReturnValue = "";
+        
+            if (menuItem.MenuItemAction != null)
+            {
+                menuReturnValue = menuItem.MenuItemAction();
+            }
+            
+            if (menuItem.Shortcut == "R")
+            {
+                return menuItem.Shortcut;
+            }
+            
+            if (menuItem.Shortcut == "E" || menuReturnValue == "E")
+            {
+                return "E";
+            }
+            
+            if ((menuItem.Shortcut == "M" || menuReturnValue == "M") && _menuLevel != EMenuLevel.Main)
+            {
+                return menuItem.Shortcut;
+            }
+
+            //return menuItem.Shortcut;
+            
+        } while (true);
+    }
+
+    private MenuItem DisplayMenuGetUserChoice()
+    {
+        var userInput = "";
+
+        do
+        {
+            DrawMenu();
+            
+            userInput = Console.ReadLine();
+            
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("Please choose an option.");
+                Console.WriteLine();
+            }
+            else
+            {
+                userInput = userInput.ToUpper();
+                
+                foreach (var menuItem in MenuItems)
+                {
+                    if (menuItem.Shortcut.ToUpper() != userInput) continue;
+                    return menuItem;
+                }
+                
+                Console.WriteLine("Please choose a valid option.");
+                Console.WriteLine();
+            }
+            
+        } while (true);
+    }
+
+    private void DrawMenu()
+    {
+        Console.WriteLine(MenuHeader);
+        Console.WriteLine(_menuDivider);
+        
+        foreach (var t in MenuItems)
+        {
+            Console.WriteLine(t);
+        }
+        
+        Console.WriteLine();
+        Console.Write(">");
+    }
+}
