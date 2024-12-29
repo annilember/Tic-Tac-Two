10730fcb6f443f5eb6bc7087e4dea61c2dec9155
Anni Lember <annlem@taltech.ee>
2024-12-29 22:18:04 +0200

    Index, Privacy and Rules page visuals

--- Tic-Tac-Two/WebApp/Pages/Index.cshtml.cs
+++ Tic-Tac-Two/WebApp/Pages/Index.cshtml.cs
@@ -7,17 +7,19 @@ namespace WebApp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    
-    private readonly AppDbContext _context;
 
     public IndexModel(ILogger<IndexModel> logger, AppDbContext context)
     {
         _logger = logger;
-        _context = context;
     }
-
-    public void OnGet()
+    
+    public Task<IActionResult> OnPostNewGame()
     {
-
+        return Task.FromResult<IActionResult>(RedirectToPage("./NewGame"));
+    }
+    
+    public Task<IActionResult> OnPostLoadGame()
+    {
+        return Task.FromResult<IActionResult>(RedirectToPage("./LoadGame"));
     }
 }
