f555730218f8b0c440b0663d368ac0a330301331
annilember <148361191+annilember@users.noreply.github.com>
2025-01-04 15:09:10 +0200

    Delete DbDemo directory

--- DbDemo/DAL/FileHelper.cs
+++
@@ -1,9 +0,0 @@
-namespace DAL;
-
-public static class FileHelper
-{
-    public static readonly string BasePath = Environment
-                                                 .GetFolderPath(Environment.SpecialFolder.UserProfile)
-                                             + Path.DirectorySeparatorChar + "Tic-Tac-Two" + Path.DirectorySeparatorChar;
-    
-}
